4d04c7dfda78c941dc0ffe1973748fdc778c912a
JungwooYang <euistgh@gmail.com>
2021-10-22 17:03:55 +0900

    clock finished

--- js/clock.js
+++ js/clock.js
@@ -1,2 +1,17 @@
-const clock = document.querySelector("#clock");
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours =  String(date.getHours()).padStart(2,"0");
+    const minutes =  String(date.getMinutes()).padStart(2, "0");
+    const seconds =  String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`
+    hours.padStart(2, "0");
+    minutes.padStart(2, "0");
+    seconds.padStart(2, "0");
+}
+
+getClock();
+setInterval(getClock, 1000);
+
 
