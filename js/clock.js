ebfc84bb156d9a3e890ebb76c8e76e39ae59098e
JungwooYang <euistgh@gmail.com>
2021-10-29 00:34:16 +0900

    todolist done

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ function getClock() {
     const hours =  String(date.getHours()).padStart(2,"0");
     const minutes =  String(date.getMinutes()).padStart(2, "0");
     const seconds =  String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
     hours.padStart(2, "0");
     minutes.padStart(2, "0");
     seconds.padStart(2, "0");
